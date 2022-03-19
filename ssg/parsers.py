654193b8bdf56e6050915c75b4f8833394c8606a
Marco <elfoscur@gmail.com>
2022-03-19 11:44:40 +0100

    fixes

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -38,13 +38,13 @@ class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        content = Content.load(self.read(path))    
-
+        content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
-
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
-
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
+        
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
 
@@ -54,6 +54,6 @@ class ReStructuredTextParser(Parser):
         html = publish_parts(content.body, writer_name = "html5")
         self.write(path, dest, html["html_body"])
 
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
