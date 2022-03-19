9a36b749e5d6c128a62c1374ecc07a0c64146c14
Marco <elfoscur@gmail.com>
2022-03-19 11:37:28 +0100

    unit 5

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from tkinter.messagebox import NO
+import sys
 
 class Site:
     def __init__(self, source, dest, parsers = None):
@@ -32,6 +33,9 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest )
         else:
-            print("Not Implementd")
+            self.error("No parser for the {} extension, file skipped!").format(path.suffix)
         
     
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n").format(message)
